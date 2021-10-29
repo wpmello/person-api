eecd0deb8a70403fe0b833d5534bb154394078ff
Wagner Mello <wagnermello903@gmail.com>
2021-10-29 18:23:59 -0300

    Method listAll

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/person")
@@ -23,8 +24,8 @@ public class PersonController {
     }
 
     @GetMapping
-    public String getPerson() {
-        return "Test rest!";
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
     }
 
     @PostMapping
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -8,6 +8,9 @@ import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
@@ -27,5 +30,10 @@ public class PersonService {
         Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder()
                 .message("Created person with ID " + savedPerson.getId()).build(); // <---- O resultado do annotation '@Builder' na classe
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPerson = personRepository.findAll();
+        return allPerson.stream().map(personMapper::toDTO).collect(Collectors.toList());
     }
 }
